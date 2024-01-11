31881bb63a49bd47d784be547d76156fdd55229f
SquallStrifeX <squallstrifex@gmail.com>
2024-01-12 00:04:57 +0100

    end exercise

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -13,7 +13,7 @@ export default {
 
 <template lang="">
 
-<div class="container">
+<div class="container container_main pt-5">
     <div class="row">
         <div class="col-6">
                 <select class="form-select" aria-label="Default select example" id="filter" v-model="store.archetype">
@@ -21,15 +21,16 @@ export default {
   <option v-for="archetipo, index in store.Archetipo" :key="index" >
         {{archetipo.archetype_name}}</option>
 </select>
-<div class="col-6">
-<button class="btn btn-primary" @click="$emit('perform_search')" > ok</button>
 </div>
-        </div>
+<div class="col-5">
+<button class="btn btn-primary" @click="$emit('perform_search')" > Ricerca</button>
+</div>
     </div>
 </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../style/generals.scss' as *;
-    
+.container_main{
+        max-width: 991px;}
 </style>
